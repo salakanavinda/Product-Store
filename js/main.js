47bdebda2bc3559916310d1e50be03730965fb58
Salaka Navinda <mmsnavinda@gmail.com>
2020-03-29 20:43:31 +0530

    Added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-$('.slider-one').slick();
+$('.slider-one')
+.not(".slick-intialized")
+.slick(
+    {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        prevArrow:"",
+        nextArrow:""
+    }
+);
