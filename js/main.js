ab791576b496707bf303acb74aaa388ee1f9333b
Salaka Navinda <mmsnavinda@gmail.com>
2020-03-31 13:02:29 +0530

    First Slider completed and second slide start

--- js/main.js
+++ js/main.js
@@ -2,12 +2,10 @@ $('.slider-one')
 .not(".slick-intialized")
 .slick(
     {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        prevArrow:".prev",
-        nextArrow:".next",
-        dots: true,
+         autoplay: true,
+         autoplaySpeed: 3000,
+         prevArrow:".prev",
+         nextArrow:".next",
+         dots: true,
     }
 );
